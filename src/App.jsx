da3c2d0fe95b2af96cc6746514b28d19b049620b
shinejeweltech-techhine <shinejeweltech@gmail.com>
2025-11-20 15:41:05 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Products from './components/Products'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Products />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
